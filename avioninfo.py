3704388e5a04432a66b4284b6dceb2938e46bc2e
valeria07rive-debug <valeria07rive@gmail.com>
2026-03-03 11:35:03 -0400

    ayuda

--- avioninfo.py
+++ avioninfo.py
@@ -1,7 +1,28 @@
+# ================================
+# MÓDULO: Información de la agencia
+# Persona 1
+# ================================
+
+def mostrar_nombre_agencia():
+    print("✈️ Lotregolpe Experiences")
+
+
+def mostrar_flota():
+    print("Flota: Airbus A676")
+
+
+def mostrar_base():
+    print("Aeropuerto base: Las Américas")
+
+
+def mostrar_rutas():
+    print("Rutas disponibles:")
+    print("1. Santo Domingo - Miami")
+    print("2. Santo Domingo - Madrid")
+
+
 def mostrar_agencia():
-    print("✈️ Lotregolpe Experiences")
-    print("Flota: Airbus A676")
-    
-    def mostrar_agencia():
-    print("✈️ Lotregolpe Experiences")
-    print("Flota: Airbus A676")
+    mostrar_nombre_agencia()
+    mostrar_flota()
+    mostrar_base()
+    mostrar_rutas()
